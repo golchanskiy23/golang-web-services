ac58dd4dcc5b35d2fd5571e52a68dd7dadfdf2f1
max <golchanskiy23@gmail.com>
2025-04-10 06:01:54 +0300

    Implement structure of codegenerator

--- 5/99_hw/codegen/handlers_gen/codegen.go
+++ 5/99_hw/codegen/handlers_gen/codegen.go
@@ -1,3 +1,52 @@
 package main
 
-// код писать тут
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+type Parser struct {
+}
+
+type CodeGenerator struct {
+}
+
+type ParsedFile struct {
+}
+
+func main() {
+	inFile, outFile := os.Args[1], os.Args[2]
+	fmt.Println(len(os.Args))
+	parser := NewParser()
+	parsedInFile, err := parser.Parse(inFile)
+	if err != nil {
+		log.Fatalf("Error happened: %s\n", err)
+	}
+	out, err := os.Create(outFile)
+	if err != nil {
+		log.Fatalf("Error creating file: %s", err)
+	}
+	defer out.Close()
+
+	codeGenerator := NewCodeGenerator(parsedInFile, out)
+	codeGenerator.Generate()
+}
+
+func NewParser() *Parser {
+	return nil
+}
+
+func (p *Parser) Parse(inFile string) (*ParsedFile, error) {
+
+	//fmt.Print(parse)
+	return nil, nil
+}
+
+func NewCodeGenerator(parsedFile *ParsedFile, out *os.File) *CodeGenerator {
+	return nil
+}
+
+func (cg *CodeGenerator) Generate() {
+
+}
